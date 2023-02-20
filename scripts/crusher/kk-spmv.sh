9901f3a4576875cf164173ab31350b2eff793bf7
Carl Pearson <pearson@illinois.edu>
2023-02-20 18:44:47 -0500

    update crusher scripts

--- scripts/crusher/kk-spmv.sh
+++ scripts/crusher/kk-spmv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -A csc465
+#SBATCH -A CSC465_crusher
 #SBATCH -p batch
-#SBATCH --time=02:00:00
-#SBATCH -o kk-smpv.o%j
-#SBATCH -e kk-smpv.e%j
+#SBATCH --time=04:00:00
+#SBATCH -o kk-spmv.o%j
+#SBATCH -e kk-spmv.e%j
 #SBATCH -J kk-spmv
 
 shopt -s extglob
@@ -16,18 +16,9 @@ source $ROOT/load-env.sh
 date
 
 echo "reals_med"
-echo "path,rows,nnz,us,GFLOPS,GB/s"
-for m in $HOME/csc465/proj-shared/cpearson/suitesparse/reals_med/*.mtx; do
-#   "$ROOT"/build-crusher/kokkos-kernels/perf_test/sparse/sparse_spmv -t kk_kernels -f $m
-  out=`"$ROOT"/build-crusher/kokkos-kernels/perf_test/sparse/sparse_spmv -t kk-kernels -f $m`
-  out=$(echo "$out" | head -n3 | tail -n1)
-  nnz=$(echo $out | tr -s ' ' | cut -d' ' -f1)
-  nr=$(echo $out | tr -s ' ' | cut -d' ' -f2)
-  avgBw=$(echo "$out" | tr -s ' ' | cut -d' ' -f6)
-  avgGf=$(echo "$out" | tr -s ' ' | cut -d' ' -f11)
-  avgMs=$(echo "$out" | tr -s ' ' | cut -d' ' -f16)
-  avgUs=$(echo $avgMs*1000 | bc)
-  echo $m,$nr,$nnz,$avgUs,$avgGf,$avgBw
-done
+"$ROOT"/build-crusher/kokkos-kernels/perf_test/sparse/KokkosKernels_spmv_benchmark \
+$HOME/csc465/proj-shared/cpearson/suitesparse/reals_med/*.mtx \
+--benchmark_filter="MatrixMarket.*f64/non-conj/.*native" \
+--benchmark_format=csv
 
 date
--- scripts/crusher/rocsparse-spmv.sh
+++ scripts/crusher/rocsparse-spmv.sh
@@ -3,9 +3,9 @@
 #SBATCH -A CSC465_crusher
 #SBATCH -p batch
 #SBATCH --time=04:00:00
-#SBATCH -o new-spmv.o%j
-#SBATCH -e new-spmv.e%j
-#SBATCH -J new-spmv
+#SBATCH -o rocsparse-spmv.o%j
+#SBATCH -e rocsparse-spmv.e%j
+#SBATCH -J rocsparse-spmv
 
 shopt -s extglob
 
@@ -18,7 +18,7 @@ date
 echo "reals_med"
 "$ROOT"/build-crusher/kokkos-kernels/perf_test/sparse/KokkosKernels_spmv_benchmark \
 $HOME/csc465/proj-shared/cpearson/suitesparse/reals_med/*.mtx \
---benchmark_filter="MatrixMarket.*f64/non-conj/.*hierarchical" \
+--benchmark_filter="MatrixMarket.*f64/non-conj/.*default" \
 --benchmark_format=csv
 
 date
